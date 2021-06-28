ad9c3cd9f6c0c7f56340f1bd1ff4d5b119c601da
Grek2722 <kirilsamus72@gmail.com>
2021-06-29 02:11:41 +0500

    fix

--- api/index.js
+++ api/index.js
@@ -16,7 +16,8 @@ const path = require("path");
 dotenv.config();
 
 mongoose.connect(
-  process.env.MONGO_URL,
+   process.env.MONGODB_URI,
+  // process.env.MONGO_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("Connected to MongoDB");
